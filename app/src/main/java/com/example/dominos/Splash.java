71dc08589927c841a2cab6482e3f11e064b9ef31
shashank s p <shashankprince523@gmail.com>
2022-05-26 17:16:35 +0530

    Domino's Pizza App

---
+++ app/src/main/java/com/example/dominos/Splash.java
@@ -0,0 +1,27 @@
+package com.example.dominos;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1200);
+    }
+}
